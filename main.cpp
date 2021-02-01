c66125e99a647350729187dcb05a2d85dae4f96d
Leah Riffell <leah.riffell@gmail.com>
2021-02-01 11:52:42 -0700

    Add madlib

--- main.cpp
+++ main.cpp
@@ -7,8 +7,22 @@
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+int main() {
+    
+    string color, pluralNoun, celebrity;
+    
+    cout << "enter a color: ";
+    getline(cin, color);
+    cout << "enter a plural noun: ";
+    getline(cin, pluralNoun);
+    cout << "enter a celebrity: ";
+    getline(cin, celebrity);
+    
+    cout << "Roses are " << color << endl;
+    cout << pluralNoun << " are blue" << endl;
+    cout << "I love " << celebrity << endl;
+    
     return 0;
 }
