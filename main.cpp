3e03dbffe166ced3fb16bc2c9f20842892b97d14
Leah Riffell <leah.riffell@gmail.com>
2021-02-01 11:44:03 -0700

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  cppPractice
+//
+//  Created by Leah Riffell on 2/1/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
